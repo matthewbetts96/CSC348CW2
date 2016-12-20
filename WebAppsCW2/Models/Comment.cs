dc836a3ef06f7bd3d2483e65714985fe57a7b372
mbetts <matthew.betts.96@gmail.com>
2016-12-20 00:25:54 +0000

    added scaffold for later use and turned off custom errors

--- WebAppsCW2/Models/Comment.cs
+++ WebAppsCW2/Models/Comment.cs
@@ -8,13 +8,16 @@ namespace WebAppsCW2.Models
 {
     public class Comment
     {
+        [ScaffoldColumn(false)]
         public int ID { get; set; }
 
         [Required(ErrorMessage = "You need to include a comment!")]
-        public string CommentBody { get; set; } 
+        public string CommentBody { get; set; }
 
+        [ScaffoldColumn(false)]
         public ApplicationUser User { get; set; }
 
+        [ScaffoldColumn(false)]
         public virtual Announcement AnnounceID { get; set; }
     }
 }
